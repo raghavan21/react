28e421f01c6be7ccdababc2a59e3d84c6cf2a0c1
Raghavan <66511693+raghavan21@users.noreply.github.com>
2023-06-09 06:49:11 +0000

    New react changes

---
+++ photo-gallery-app/src/content/data.js
@@ -0,0 +1,66 @@
+
+export const data = [
+  {
+    "name": "Photo 1",
+    "url": "/images/1.png",
+    "description": "Photo 1 description",
+    "date": "2020-01-01",
+    "type": "image/png",
+  },
+  {
+    "name": "Photo 2",
+    "url": "/images/2.png",
+    "description": "Photo 2 description",
+    "date": "2020-01-02",
+    "type": "image/png",
+  },
+  {
+    "name": "Photo 3",
+    "url": "/images/3.png",
+    "description": "Photo 3 description",
+    "date": "2020-01-03",
+    "type": "image/png",
+  },
+  {
+    "name": "Photo 4",
+    "url": "/images/4.png",
+    "description": "Photo 4 description",
+    "date": "2020-01-04",
+    "type": "image/png",
+  },
+  {
+    "name": "Photo 5",
+    "url": "/images/5.png",
+    "description": "Photo 5 description",
+    "date": "2020-01-05",
+    "type": "image/png",
+  },
+  {
+    "name": "Photo 6",
+    "url": "/images/6.png",
+    "description": "Photo 6 description",
+    "date": "2020-01-06",
+    "type": "image/png",
+  },
+  {
+    "name": "Photo 7",
+    "url": "/images/7.png",
+    "description": "Photo 7 description",
+    "date": "2020-01-07",
+    "type": "image/png",
+  },
+  {
+    "name": "Photo 8",
+    "url": "/images/8.png",
+    "description": "Photo 8 description",
+    "date": "2020-01-08",
+    "type": "image/png",
+  },
+  {
+    "name": "Photo 9",
+    "url": "/images/9.png",
+    "description": "Photo 9 description",
+    "date": "2020-01-09",
+    "type": "image/png",
+  }
+]
